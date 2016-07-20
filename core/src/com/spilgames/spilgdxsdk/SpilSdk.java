b3be6dffe436f9d9b48e63bfaf634750c1dc1811
piotr-j <me@piotrjastrzebski.io>
2016-07-20 17:14:04 +0200

    added strings for providers and ad types

--- core/src/com/spilgames/spilgdxsdk/SpilSdk.java
+++ core/src/com/spilgames/spilgdxsdk/SpilSdk.java
@@ -8,6 +8,13 @@ import com.badlogic.gdx.utils.ObjectMap;
  * Created by PiotrJ on 01/07/16.
  */
 public interface SpilSdk {
+	String PROVIDER_CHARTBOOST = "ChartBoost";
+	String PROVIDER_DFP = "DFP";
+	String PROVIDER_FYBER = "Fyber";
+	String AD_INTERSTITIAL = "interstitial";
+	String AD_REWARD_VIDEO = "rewardVideo";
+	String AD_MORE_APPS = "moreApps";
+
 	SpilSdkType getBackendType ();
 
 	void setDebug(boolean debug);

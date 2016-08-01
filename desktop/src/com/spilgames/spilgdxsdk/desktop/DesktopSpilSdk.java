d6b4b17573f6a38a5b243e8a31175ea6354e51a8
piotr-j <me@piotrjastrzebski.io>
2016-08-01 20:37:24 +0200

    added option to toggle logging

--- desktop/src/com/spilgames/spilgdxsdk/desktop/DesktopSpilSdk.java
+++ desktop/src/com/spilgames/spilgdxsdk/desktop/DesktopSpilSdk.java
@@ -2,8 +2,6 @@ package com.spilgames.spilgdxsdk.desktop;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.JsonValue;
-import com.badlogic.gdx.utils.JsonWriter;
-import com.badlogic.gdx.utils.ObjectMap;
 import com.spilgames.spilgdxsdk.*;
 
 /**
@@ -13,15 +11,22 @@ import com.spilgames.spilgdxsdk.*;
  */
 public class DesktopSpilSdk implements SpilSdk {
 	private final static String TAG = DesktopSpilSdk.class.getSimpleName();
-	private JsonValue config = new JsonValue(JsonValue.ValueType.object);
+	private boolean log;
 
 	public DesktopSpilSdk() {
-		config.child = new JsonValue("dummyData");
-		config.child.name = "dummyKey";
+		this(true);
 	}
 
-	public void setConfig(JsonValue config) {
-		this.config = config;
+	public DesktopSpilSdk(boolean loggingEnabled) {
+		setLogging(loggingEnabled);
+	}
+
+	public void setLogging (boolean enabled) {
+		this.log = enabled;
+	}
+
+	public boolean isLogging () {
+		return log;
 	}
 
 	@Override public SpilSdkType getBackendType () {
@@ -29,49 +34,53 @@ public class DesktopSpilSdk implements SpilSdk {
 	}
 
 	@Override public void requestMoreApps () {
-		Gdx.app.log(TAG, "requestMoreApps");
+		log(TAG, "requestMoreApps");
+	}
+
+	private void log (String tag, String message) {
+		if (log) Gdx.app.log(tag, message);
 	}
 
 	@Override public void requestRewardVideo () {
-		Gdx.app.log(TAG, "requestRewardVideo");
+		log(TAG, "requestRewardVideo");
 	}
 
 	@Override public void setDebug (boolean debug) {
-		Gdx.app.log(TAG, "setDebug ("+debug+")");
+		log(TAG, "setDebug ("+debug+")");
 	}
 
 	@Override public void trackEvent (SpilEvent event) {
-		Gdx.app.log(TAG, "trackEvent ("+event+")");
+		log(TAG, "trackEvent ("+event+")");
 	}
 
 	@Override public void trackEvent (SpilEvent event, SpilEventActionListener listener) {
-		Gdx.app.log(TAG, "trackEvent ("+event+", "+listener+")");
+		log(TAG, "trackEvent ("+event+", "+listener+")");
 	}
 
 	@Override public void setSpilRewardListener (SpilRewardListener rewardListener) {
-		Gdx.app.log(TAG, "SpilRewardListener ("+rewardListener+")");
+		log(TAG, "SpilRewardListener ("+rewardListener+")");
 	}
 
 	@Override public JsonValue getConfig () {
-		return config;
+		return null;
 	}
 
 	@Override public void requestPackages () {
-		Gdx.app.log(TAG, "requestPackages");
+		log(TAG, "requestPackages");
 	}
 
 	@Override public JsonValue getPromotion (String packageId) {
-		Gdx.app.log(TAG, "getPromotion");
+		log(TAG, "getPromotion");
 		return null;
 	}
 
 	@Override public JsonValue getPackage (String packageId) {
-		Gdx.app.log(TAG, "getPackage");
+		log(TAG, "getPackage");
 		return null;
 	}
 
 	@Override public JsonValue getAllPackages () {
-		Gdx.app.log(TAG, "getAllPackages");
+		log(TAG, "getAllPackages");
 		return null;
 	}
 
@@ -80,96 +89,96 @@ public class DesktopSpilSdk implements SpilSdk {
 	}
 
 	@Override public void showMoreApps () {
-		Gdx.app.log(TAG, "showMoreApps");
+		log(TAG, "showMoreApps");
 	}
 
 	@Override public void showRewardVideo () {
-		Gdx.app.log(TAG, "showRewardVideo");
+		log(TAG, "showRewardVideo");
 	}
 
 	@Override public void setSpilAdCallbacks (SpilAdCallbacks adCallbacks) {
-		Gdx.app.log(TAG, "setSpilAdCallbacks ("+adCallbacks+")");
+		log(TAG, "setSpilAdCallbacks ("+adCallbacks+")");
 	}
 
 	@Override public void devRequestAd (String provider, String adType, boolean parentalGate) {
-		Gdx.app.log(TAG, "devRequestAd ("+provider+", "+adType+", "+parentalGate+")");
+		log(TAG, "devRequestAd ("+provider+", "+adType+", "+parentalGate+")");
 	}
 
 	@Override public void devShowRewardVideo (String provider) {
-		Gdx.app.log(TAG, "devShowRewardVideo ("+provider+")");
+		log(TAG, "devShowRewardVideo ("+provider+")");
 	}
 
 	@Override public void devShowInterstitial (String provider) {
-		Gdx.app.log(TAG, "devShowInterstitial ("+provider+")");
+		log(TAG, "devShowInterstitial ("+provider+")");
 	}
 
 	@Override public void devShowMoreApps (String provider) {
-		Gdx.app.log(TAG, "devShowMoreApps ("+provider+")");
+		log(TAG, "devShowMoreApps ("+provider+")");
 	}
 
 	@Override public void requestGameData () {
-		Gdx.app.log(TAG, "requestGameData");
+		log(TAG, "requestGameData");
 	}
 
 	@Override public void requestPlayerData () {
-		Gdx.app.log(TAG, "requestPlayerData)");
+		log(TAG, "requestPlayerData)");
 	}
 
 	@Override public void setSpilPlayerDataListener (SpilPlayerDataListener playerDataListener) {
-		Gdx.app.log(TAG, "setSpilPlayerDataListener ("+playerDataListener+")");
+		log(TAG, "setSpilPlayerDataListener ("+playerDataListener+")");
 	}
 
 	@Override public void setSpilGameDataListener (SpilGameDataListener gameDataListener) {
-		Gdx.app.log(TAG, "setSpilGameDataListener ("+gameDataListener+")");
+		log(TAG, "setSpilGameDataListener ("+gameDataListener+")");
 	}
 
 	@Override public JsonValue getUserProfile () {
-		Gdx.app.log(TAG, "getUserProfile");
+		log(TAG, "getUserProfile");
 		return null;
 	}
 
 	@Override public JsonValue getWallet () {
-		Gdx.app.log(TAG, "getWallet");
+		log(TAG, "getWallet");
 		return null;
 	}
 
 	@Override public JsonValue getGameData () {
-		Gdx.app.log(TAG, "getGameData");
+		log(TAG, "getGameData");
 		return null;
 	}
 
 	@Override public JsonValue getInventory () {
-		Gdx.app.log(TAG, "getInventory");
+		log(TAG, "getInventory");
 		return null;
 	}
 
 //	@Override public String getShop () {
-//		Gdx.app.log(TAG, "getShop");
+//		log(TAG, "getShop");
 //		return null;
 //	}
 //
 //	@Override public String getShopPromotions () {
-//		Gdx.app.log(TAG, "getShopPromotions");
+//		log(TAG, "getShopPromotions");
 //		return null;
 //	}
 
 	@Override public void addCurrencyToWallet (int currencyId, int amount, String reason) {
-		Gdx.app.log(TAG, "addCurrencyToWallet (" +currencyId+", "+amount+", "+ reason+")");
+		log(TAG, "addCurrencyToWallet (" +currencyId+", "+amount+", "+ reason+")");
 	}
 
 	@Override public void subtractCurrencyFromWallet (int currencyId, int amount, String reason) {
-		Gdx.app.log(TAG, "subtractCurrencyFromWallet (" +currencyId+", "+amount+", "+ reason+")");
+		log(TAG, "subtractCurrencyFromWallet (" +currencyId+", "+amount+", "+ reason+")");
 	}
 
 	@Override public void addItemToInventory (int itemId, int amount, String reason) {
-		Gdx.app.log(TAG, "addItemToInventory (" +itemId+", "+amount+", "+ reason+")");
+		log(TAG, "addItemToInventory (" +itemId+", "+amount+", "+ reason+")");
 	}
 
 	@Override public void subtractItemFromInventory (int itemId, int amount, String reason) {
-		Gdx.app.log(TAG, "subtractItemFromInventory (" +itemId+", "+amount+", "+ reason+")");
+		log(TAG, "subtractItemFromInventory (" +itemId+", "+amount+", "+ reason+")");
 	}
 
 	@Override public void consumeBundle (int bundleId, String reason) {
-		Gdx.app.log(TAG, "consumeBundle (" +bundleId+", "+ reason+")");
+		log(TAG, "consumeBundle (" +bundleId+", "+ reason+")");
 	}
 }
